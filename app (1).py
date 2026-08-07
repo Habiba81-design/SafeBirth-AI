fa73317de8efeaf43c9bf79acd8d6cc48acef453
Lambon Jnr. <lambonhabibatu8@gmail.com>
2026-08-07 16:28:13 +0100

    Update demo.launch to specify server details

--- app (1).py
+++ app (1).py
@@ -286,4 +286,8 @@ with gr.Blocks(title="SafeBirth Prototype") as demo:
     )
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860))
+    )
+
